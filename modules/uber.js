6ca08b0dcd1c16e6fd70b6bc8ab907dee49960fe
Shivam Shah <dev.shivamshah@gmail.com>
2015-02-07 01:27:18 +0530

    Added Uber Cost endpoint and parsing duration to 2 decimal places API Usage: http://localhost:3000/cabs/uber/cost?srcLat=12.909521699999999&srcLng=77.61116199999992&destLat=12.932652&destLng=77.68362

--- modules/uber.js
+++ modules/uber.js
@@ -8,42 +8,83 @@ UBER.options = {
     path: ''
 };
 
-function buildURL(latitude, longitude, userId) {
+function buildEstimateURL(latitude, longitude, userId) {
     var url = '/v1/estimates/time?server_token=RQ28hrjOR39zq2w5sof9xiTHolQ_z9t4n5T2etHP';
 
     if (latitude && longitude) {
         url += '&start_latitude=' + latitude + '&start_longitude=' + longitude
     }
 
-    console.log('UBER API url :: ' + UBER.options.host + url);
+    console.log('UBER Estimate API url :: ' + UBER.options.host + url);
     return url;
 }
 
-function parseResponse(response) {
+function buildPriceURL(srcLatitude, srcLongitude, destLatitude, destLongitude, userId) {
+    var url = '/v1/estimates/price?server_token=RQ28hrjOR39zq2w5sof9xiTHolQ_z9t4n5T2etHP';
+
+    if (srcLatitude && srcLongitude && destLatitude && destLongitude) {
+        url += '&start_latitude=' + srcLatitude + '&start_longitude=' + srcLongitude;
+        url += '&end_latitude=' + destLatitude + '&end_longitude=' + destLongitude;
+    }
+
+    console.log('UBER Cost API url :: ' + UBER.options.host + url);
+    return url;
+}
+
+function parseResponse(type, response) {
     var output = {
         status: "success"
     }
-    output.cabsEstimate = [];
 
-    for (var i = 0; i < response.times.length; i++) {
-        var _cEst = {
-            name: response.times[i].localized_display_name,
-            available: true,
-            duration: response.times[i].estimate / 60,
-            distance: null
+    if (type === 'estimate') {
+        output.cabsEstimate = [];
+
+        for (var i = 0; i < response.times.length; i++) {
+            var _cEst = {
+                name: response.times[i].localized_display_name,
+                available: true,
+                duration: parseFloat(response.times[i].estimate / 60).toFixed(2),
+                distance: null
+            }
+            output.cabsEstimate.push(_cEst);
         }
-        output.cabsEstimate.push(_cEst);
+    } else if(type === 'price') {
+        output.prices = [];
+
+        for (var i = 0; i < response.prices.length; i++) {
+            var _cEst = {
+                name: response.prices[i].localized_display_name,
+                available: true,
+                duration: parseFloat(response.prices[i].duration / 60).toFixed(2),
+                distance: response.prices[i].distance,
+                multiplier: response.prices[i].surge_multiplier,
+                low_estimate: response.prices[i].low_estimate,
+                high_estimate: response.prices[i].high_estimate
+            }
+            output.prices.push(_cEst);
+        }
     }
 
     return output;
 }
 
 exports.call = function(responseHandler, response, latitude, longitude, shouldParseData, userId) {
-    UBER.options.path = buildURL(latitude, longitude, userId);
+    UBER.options.path = buildEstimateURL(latitude, longitude, userId);
 
     request.getJSON(UBER.options, function(statusCode, result) {
         if (shouldParseData) {
-            result = parseResponse(result);
+            result = parseResponse('estimate', result);
+        }
+        responseHandler(response, result);
+    });
+}
+
+exports.price = function(responseHandler, response, srcLatitude, srcLongitude, destLatitude, destLongitude, shouldParseData, userId) {
+    UBER.options.path = buildPriceURL(srcLatitude, srcLongitude, destLatitude, destLongitude, userId);
+
+    request.getJSON(UBER.options, function(statusCode, result) {
+        if (shouldParseData) {
+            result = parseResponse('price', result);
         }
         responseHandler(response, result);
     });
--- routes/cabs.js
+++ routes/cabs.js
@@ -19,6 +19,19 @@ router.get('/:cab', function(request, response) {
     }
 });
 
+router.get('/:cab/cost', function(request, response) {
+    var srcLatitude =  request.query.srcLat;
+    var srcLongitude = request.query.srcLng;
+    var destLatitude = request.query.destLat;
+    var destLongitude = request.query.destLng;
+    var cabService = request.params.cab;
+    var shouldParseData = request.query.parseData ? (request.query.parseData == 'false' ? false : true) : true;
+
+    if (cabService && cabServiceModules[cabService]) {
+        cabServiceModules[cabService].price(sendResponse, response, srcLatitude, srcLongitude, destLatitude, destLongitude, shouldParseData);
+    }
+});
+
 function sendResponse(response, result) {
     response.json(result);
 }
