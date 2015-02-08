0e3829de3f06fb0109d9187278014db519a888d8
Shivam Shah <raze.wickedxe@yahoo.com>
2015-02-09 01:36:59 +0530

    Fix for DistanceMatrix API request failure

--- modules/globals.js
+++ modules/globals.js
@@ -1,6 +1,15 @@
-var GLOBALS = {
-    response: {},
-    cabServices: ['ola', 'tfs', 'uber']
-};
+var GOOGLE_API_KEYS = [
+    "",
+    "AIzaSyCoWnRV1a-5GZAwJFRgrQlU0xmDFJRjzVQ",
+    "AIzaSyBLRq6bkEP8ZiJ0BBfFQ78Bq2LzVjX3-o8",
+    "AIzaSyBbAX5JrFJ8Y6YqLV84-ex52z9tBGJd92o",
+    "AIzaSyAVWw8oS6JCdAIwqes8xKC5SFZ5GFzualM",
+    "AIzaSyA40uFvZXdxaVkmXiK6Rldo7NBcY-lKLOk"
+];
 
-module.exports = GLOBALS
+exports.getGoogleAPIKeys = function(development) {
+	if (development) {
+		return ["AIzaSyBbAX5JrFJ8Y6YqLV84-ex52z9tBGJd92o"];
+	}
+	return GOOGLE_API_KEYS;
+}
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,19 @@
 var express = require('express');
+var globals = require('../modules/globals');
+
 var router = express.Router();
+var APIKeys = globals.getGoogleAPIKeys();
+var API_INDEX = 0;
 
 /* GET home page. */
 router.get('/', function(req, res) {
+	var mapUrl = 'https://maps.googleapis.com/maps/api/js?libraries=places';
+	mapUrl += (APIKeys[API_INDEX] === "" ? ("&key=" + APIKeys[API_INDEX+1]) : ("&key=" + APIKeys[API_INDEX]));
+    API_INDEX = (API_INDEX + 1) % (APIKeys.length);
+
     res.render('index', {
-        title: 'TaxiStop'
+        title: 'TaxiStop',
+        gmaps: mapUrl
     });
 });
 
